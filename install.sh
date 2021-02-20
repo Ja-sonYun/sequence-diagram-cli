e63282f6d7e8aa7240e6be2fb2e6af2abee059ee
Ja-sonYun <killa30867@gmail.com>
2021-02-21 07:57:38 +0900

    v1.2.1

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ fi
 wget https://github.com/Ja-sonYun/sequence-diagram-cli/blob/main/seqdia\?raw\=true -P ~/.seqdia
 
 echo "\x1B[32m** Moving binary file to '/usr/local/bin'.\033[0m"
-mv ~/.seqdia/seqdia?raw=true /usr/local/bin/seqdia
+sudo mv ~/.seqdia/seqdia?raw=true /usr/local/bin/seqdia
 
 echo "\x1B[32m** chmod 755 /usr/local/bin/seqdia...\033[0m"
 sudo chmod 755 /usr/local/bin/seqdia
