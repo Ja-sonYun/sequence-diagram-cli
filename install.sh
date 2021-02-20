307e2b32939d941dc79a0c3e7a9910ba5a56a7b6
Ja-sonYun <killa30867@gmail.com>
2021-02-21 07:55:07 +0900

    v1.2.1

--- install.sh
+++ install.sh
@@ -10,29 +10,30 @@ fi
 
 wget https://github.com/Ja-sonYun/sequence-diagram-cli/blob/main/seqdia\?raw\=true -P ~/.seqdia
 
-echo "\x1B[32m** chmod 755 /usr/local/bin/seqdia...\033[0m"
-sudo chmod 755 /usr/local/bin/seqdia
-
-if [ "$EUID" -ne 0 ]
-  then echo "Installation canceled."
-    echo "\x1B[32m** Removing temp folder ~/.seqdia\033[0m"
-    rm -rf ~/.seqdia
-  exit
-fi
-
 echo "\x1B[32m** Moving binary file to '/usr/local/bin'.\033[0m"
 mv ~/.seqdia/seqdia?raw=true /usr/local/bin/seqdia
+
+echo "\x1B[32m** chmod 755 /usr/local/bin/seqdia...\033[0m"
+sudo chmod 755 /usr/local/bin/seqdia
 
 echo "\x1B[32m** Removing temp folder ~/.seqdia\033[0m"
 rm -rf ~/.seqdia
 
 if [ -f "/usr/local/bin/seqdia" ]
 then
-    echo "\x1B[32m** Installed!\033[0m"
+    :
 else
     echo "\x1B[33mWARNING: something is wrong! Please move binary file to your PATH manually."
     echo "\x1B[33mWARNING: you can also compile this package from github source."
     echo "\x1B[33mWARNING: https://github.com/Ja-sonYun/sequence-diagram-cli\033[0m"
+    exit 1
+fi
+
+if [ -x "/usr/local/bin/seqdia" ]
+then
+    echo "\x1B[32m** Installed!\033[0m"
+else
+    echo "\x1B[33mPermission Error! Please run \`sudo chmod 755 /usr/local/bin/seqdia\`"
     exit 1
 fi
 
