b8c0501af0e23331637297f12539f3c2c335efbc
Ja-sonYun <killa30867@gmail.com>
2021-03-15 11:22:45 +0900

    1.3.4 bug fixed

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ fi
 
 D=`uname -a | awk '{print $1}'`
 if [ "$D" = "Darwin" ]; then
-    wget https://github.com/Ja-sonYun/sequence-diagram-cli/releases/download/v1.3.3/Darwin_seqdia -P ~/.seqdia
+    wget https://github.com/Ja-sonYun/sequence-diagram-cli/releases/download/v1.3.4/Darwin_seqdia -P ~/.seqdia
     mv ~/.seqdia/Darwin_seqdia ~/.seqdia/seqdia
-    CHECKSUMD="070ced401cd599f43d7cfcabc78ed8ab"
+    CHECKSUMD="43c081ef5521c39c6e1d6a0dd129dd5d"
 elif [ "$D" = "Linux" ]; then
     echo "Clone repository..."
     git clone https://github.com/Ja-sonYun/sequence-diagram-cli.git ~/.seqdia
